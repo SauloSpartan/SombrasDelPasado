e473c29225a6cb18d24fcd0bacb7c889d04fc2ec
SauloTrujillo <SauloSpartan2@gmail.com>
2021-11-07 15:48:16 -0600

    Barra de Vida
    El personaje ya cuenta con una barra de vida que va cambiando su color de verde a rojo conforme esta baja

--- Sombras del Pasado/Assets/Scripts/Canvas/HealthBar.cs
+++ Sombras del Pasado/Assets/Scripts/Canvas/HealthBar.cs
@@ -8,6 +8,7 @@ public class HealthBar : MonoBehaviour
     private Image healthBar;
     public float currentHealth;
     private float maxHealth = 100f;
+    private float lerpSpeed;
     ControllerCharacter1 Player;
 
     private void Start()
@@ -19,7 +20,25 @@ public class HealthBar : MonoBehaviour
 
     private void Update()
     {
+        HealthControl();
+
+        lerpSpeed = 3f * Time.deltaTime;
+
+        healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, currentHealth / maxHealth, lerpSpeed);
+
+        ColorChanger();
+    }
+
+    private void HealthControl()
+    {
         currentHealth = Player.health;
         healthBar.fillAmount = currentHealth / maxHealth;
+    }
+
+    private void ColorChanger()
+    {
+        Color healthColor = Color.Lerp(Color.red, Color.green, (currentHealth / maxHealth));
+
+        healthBar.color = healthColor;
     }
 }
