33f3fce84a457039f12235bc35c57695b457d734
SauloTrujillo <SauloSpartan2@gmail.com>
2021-11-28 19:41:03 -0600

    Configuración de Audio
    Ya se puede editar el volumen de los sonidos del juego

--- Sombras del Pasado/Assets/Scripts/Canvas/MainMenu.cs
+++ Sombras del Pasado/Assets/Scripts/Canvas/MainMenu.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class MainMenu : MonoBehaviour
 {
     public int buildIndex;
+    public AudioMixer audioMixer;
 
     void Start()
     {
         buildIndex = SceneManager.GetActiveScene().buildIndex;
     }
 
-    // Update is called once per frame
     void Update()
     {
         
@@ -26,5 +27,10 @@ public class MainMenu : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
     }
 }
