342ff7e165c50d2406e30b5caa1834db285dcbed
SauloTrujillo <SauloSpartan2@gmail.com>
2022-11-04 08:45:56 -0600

    Spawn de Enemigos - Mitad
    El spawn de enemigos funciona completamente, solo se necesitan ligeros arreglos.

--- Sombras del Pasado/Assets/Scripts/World/SpawnEnemy.cs
+++ Sombras del Pasado/Assets/Scripts/World/SpawnEnemy.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class SpawnEnemy : MonoBehaviour
 {
+    // Wall variables
+    [Header("Wall Spacing")]
     [SerializeField] private Vector3 _offset;
     private Vector3 _spawnerPosition;
     private Vector3 _rightWall;
     private Vector3 _leftWall;
-
     private Transform _rightCollider;
     private Transform _leftCollider;
+
+    [Header("Spawn Control")]
+    [SerializeField] private float _spawnTimer; 
+    [SerializeField] private GameObject _enemyBasic;
+    [SerializeField] private int _basicQuantity;
+    private Vector3 _enemySpawnRight;
+    private Vector3 _enemySpawnLeft;
 
     void Awake()
     {
@@ -24,6 +32,12 @@ public class SpawnEnemy : MonoBehaviour
 
     void Update()
     {
+        WallSpacing();
+        SpawnSpacing();
+    }
+
+    private void WallSpacing()
+    {
         _spawnerPosition = transform.position;
         _rightWall = _spawnerPosition + _offset;
         _leftWall = _spawnerPosition - _offset;
@@ -32,12 +46,40 @@ public class SpawnEnemy : MonoBehaviour
         _leftCollider.transform.position = _leftWall;
     }
 
+    private void SpawnSpacing()
+    {
+        _enemySpawnRight = _spawnerPosition + (_offset * 3);
+        _enemySpawnLeft = _spawnerPosition - (_offset * 3);
+    }
+
+    private IEnumerator SpawnControl()
+    {
+        int randomSpawner = 0;
+        Vector3 randomPosition = _spawnerPosition;
+
+        for (int i = 0; i < _basicQuantity; i++)
+        {
+            randomSpawner = Random.Range(0, 2);
+            if (randomSpawner == 0)
+            {
+                randomPosition = _enemySpawnRight;
+            }
+            else if (randomSpawner == 1)
+            {
+                randomPosition = _enemySpawnLeft;
+            }
+            Instantiate(_enemyBasic, randomPosition, transform.rotation);
+            yield return new WaitForSecondsRealtime(_spawnTimer);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
             _rightCollider.transform.gameObject.SetActive(true);
             _leftCollider.transform.gameObject.SetActive(true);
+            StartCoroutine(SpawnControl());
         }
     }
 
@@ -50,5 +92,13 @@ public class SpawnEnemy : MonoBehaviour
         ColorUtility.TryParseHtmlString("#ff8400", out orange);
         Gizmos.color = orange;
         Gizmos.DrawLine(_spawnerPosition, _leftWall);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(_enemySpawnRight, Vector3.one);
+
+        Color green2;
+        ColorUtility.TryParseHtmlString("#00ff7e", out green2);
+        Gizmos.color = green2;
+        Gizmos.DrawWireCube(_enemySpawnLeft, Vector3.one);
     }
 }
